fb019e17eb4385499790a45ff2c25e2433d05a52
Akihiro Nagami <nagami@kikyos.co.jp>
2019-02-16 21:12:47 +0900

    Initial Commit

---
+++ WebView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebView
+//
+//  Created by 永見彰宏 on 2019/02/16.
+//  Copyright © 2019 永見彰宏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
