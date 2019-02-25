638788ea31fed01bd6b18ce30f317ac62246eba0
Akihiro Nagami <nagami@kikyos.co.jp>
2019-02-25 20:59:50 +0900

    ボタンクリックが行われない

--- WebView/ViewController.swift
+++ WebView/ViewController.swift
@@ -1,20 +1,35 @@
-//
-//  ViewController.swift
-//  WebView
-//
-//  Created by 永見彰宏 on 2019/02/16.
-//  Copyright © 2019 永見彰宏. All rights reserved.
-//
-
 import UIKit
-
-class ViewController: UIViewController {
-
+import WebKit
+class ViewController: UIViewController, WKUIDelegate ,WKNavigationDelegate{
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        view = webView
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let myURL = URL(string: "https://transit.yahoo.co.jp")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
     }
+    
 
 
+func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+    print("遷移開始")
 }
 
+func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+    print("読み込み完了")
+    webView.evaluateJavaScript("document.getElementById(\"sfrom\").value=\"開始駅\"", completionHandler: nil)
+    webView.evaluateJavaScript("document.getElementById(\"sto\").value=\"到着駅\"", completionHandler: nil)
+    webView.evaluateJavaScript("document.getElementById(\"searchModuleSubmit\").click()", completionHandler: nil)
+}
+
+}
